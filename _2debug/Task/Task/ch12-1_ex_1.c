ff3f11ab7217ee91a0a3f066b271182d216442c9
zerowon <songyoungwon1234.1234@gmail.com>
2026-09-05 13:46:04 +0900

    출력 결과 띄어쓰기

--- _2debug/Task/Task/ch12-1_ex_1.c
+++ _2debug/Task/Task/ch12-1_ex_1.c
@@ -12,8 +12,8 @@ int main()
 	char c = 69;
 	float f = 12.3;
 	
-	printf("i의주소: %p\n", &i);	// 변수 i의 주소 출력
-	printf("c의주소: %p\n", &c);	// 변수 c의 주소 출력
-	printf("f의주소: %p\n", &f);	// 변수 f의 주소 출력
+	printf("i의 주소: %p\n", &i);		// 변수 i의 주소 출력
+	printf("c의 주소: %p\n", &c);		// 변수 c의 주소 출력
+	printf("f의 주소: %p\n", &f);		// 변수 f의 주소 출력
 	
 }
