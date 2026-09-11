b80faba82f7280c9a9a2cca1ca8f7f78703fbd1a
zerowon <songyoungwon1234.1234@gmail.com>
2026-09-11 14:26:04 +0900

    ch13-1_4, 5 수정

--- _2debug/Task/Task/ch13-1_5.c
+++ _2debug/Task/Task/ch13-1_5.c
@@ -13,7 +13,7 @@ int main(void)
 	for (int i = 0; i < 5; i++)
 	{
 		printf("arr[%d] 초기화: ", i);
-		scanf("%f", (ptrArr + i));
+		scanf("%f", ptrArr + i);
 	}
 
 	for (int i = 0; i < 5; i++)
@@ -29,3 +29,30 @@ int main(void)
 	printf("}\n");
 
 }
+
+//int main(void)
+//{
+//	int a[] = { 10, 20, 30 }, i, sum = 0;
+//	int* p = a;
+//
+//	for (i = 0; i < 3; i++)
+//		sum += a[i]; //배열표현
+//	printf("sum:%d\n", sum);
+//	sum = 0;
+//	for (i = 0; i < 3; i++)
+//		sum += *(a + i); //배열명을 이용한 주소표현
+//	printf("sum:%d\n", sum);
+//	sum = 0;
+//	for (i = 0; i < 3; i++)
+//		sum += p[i]; //포인터변수를 이용한 배열표현
+//	printf("sum:%d\n", sum);
+//	sum = 0;
+//	for (i = 0; i < 3; i++)
+//		sum += *(p + i); //포인터 변수를 이용한 주소표현
+//	printf("sum:%d\n", sum);
+//	sum = 0;
+//	for (i = 0; i < 3; i++)
+//		sum += *p++; //증감연산자 이용한 주소표현
+//	printf("sum:%d\n", sum);
+//	return 0;
+//}
